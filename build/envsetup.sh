5c94350b827dec31dfea11e5c4e9a6ad0fd8e98d
樊春春 <chunchun.fan@rntec.net>
2025-04-23 17:26:04 +0800

    新增一部分文件

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -12,7 +12,7 @@ function choose_board()
 	echo "Lunch menu...pick a combo:"
 	echo ""
 
-	echo "0. non-rockchip boards"
+	echo "0. non-imx6ull boards"
 	echo ${RK_DEFCONFIG_ARRAY[@]} | xargs -n 1 | sed "=" | sed "N;s/\n/. /"
 
 	local INDEX
@@ -21,15 +21,15 @@ function choose_board()
 		INDEX=$((${INDEX:-0} - 1))
 
 		if [ "$INDEX" -eq -1 ]; then
-			echo "Lunching for non-rockchip boards..."
+			echo "Lunching for non-imx6ull boards..."
 			unset TARGET_OUTPUT_DIR
-			unset RK_BUILD_CONFIG
+			unset IMX_BUILD_CONFIG
 			break;
 		fi
 
 		if echo $INDEX | grep -vq [^0-9]; then
-			RK_BUILD_CONFIG="${RK_DEFCONFIG_ARRAY[$INDEX]}"
-			[ -n "$RK_BUILD_CONFIG" ] && break
+			IMX_BUILD_CONFIG="${RK_DEFCONFIG_ARRAY[$INDEX]}"
+			[ -n "$IMX_BUILD_CONFIG" ] && break
 		fi
 
 		echo
@@ -38,23 +38,23 @@ function choose_board()
 	done
 }
 
-function lunch_rockchip()
+function lunch_imx6ull()
 {
-	TARGET_DIR_NAME="$RK_BUILD_CONFIG"
+	TARGET_DIR_NAME="$IMX_BUILD_CONFIG"
 	export TARGET_OUTPUT_DIR="$BUILDROOT_OUTPUT_DIR/$TARGET_DIR_NAME"
 
 	mkdir -p $TARGET_OUTPUT_DIR || return
 
 	echo "==========================================="
 	echo
-	echo "#TARGET_BOARD=`echo $RK_BUILD_CONFIG | cut -d '_' -f 2`"
+	echo "#TARGET_BOARD=`echo $IMX_BUILD_CONFIG | cut -d '_' -f 2`"
 	echo "#OUTPUT_DIR=output/$TARGET_DIR_NAME"
-	echo "#CONFIG=${RK_BUILD_CONFIG}_defconfig"
+	echo "#CONFIG=${IMX_BUILD_CONFIG}_defconfig"
 	echo
 	echo "==========================================="
 
 	make -C ${BUILDROOT_DIR} O="$TARGET_OUTPUT_DIR" \
-		"$RK_BUILD_CONFIG"_defconfig
+		"$IMX_BUILD_CONFIG"_defconfig
 
 	CONFIG=${TARGET_OUTPUT_DIR}/.config
 	cp ${CONFIG}{,.new}
@@ -82,11 +82,11 @@ function main()
 	alias croot="cd ${TOP_DIR}"
 
 	RK_DEFCONFIG_ARRAY=(
-		$(cd ${BUILDROOT_DIR}/configs/; ls rockchip_* | \
+		$(cd ${BUILDROOT_DIR}/configs/; ls imx6ullevk_* | \
 			sed "s/_defconfig$//" | grep "$1" | sort)
 	)
 
-	unset RK_BUILD_CONFIG
+	unset IMX_BUILD_CONFIG
 	RK_DEFCONFIG_ARRAY_LEN=${#RK_DEFCONFIG_ARRAY[@]}
 
 	case $RK_DEFCONFIG_ARRAY_LEN in
@@ -94,23 +94,23 @@ function main()
 			echo No available configs${1:+" for: $1"}
 			;;
 		1)
-			RK_BUILD_CONFIG=${RK_DEFCONFIG_ARRAY[0]}
+			IMX_BUILD_CONFIG=${RK_DEFCONFIG_ARRAY[0]}
 			;;
 		*)
 			if [ "$1" = ${RK_DEFCONFIG_ARRAY[0]} ]; then
 				# Prefer exact-match
-				RK_BUILD_CONFIG=$1
+				IMX_BUILD_CONFIG=$1
 			else
 				choose_board
 			fi
 			;;
 	esac
 
-	[ -n "$RK_BUILD_CONFIG" ] || return
+	[ -n "$IMX_BUILD_CONFIG" ] || return
 
-	source ${TOP_DIR}/device/rockchip/.BoardConfig.mk
+	source ${TOP_DIR}/device/.BoardConfig.mk
 
-	lunch_rockchip
+	lunch_imx6ull
 }
 
 if [ "${BASH_SOURCE}" == "$0" ];then
